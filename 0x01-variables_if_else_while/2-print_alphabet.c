d58ce7690f2ecb39ddeee6bbbf4d5b20bd88f9ac
Bwari500 <dorisb.nyakundi@gmail.com>
2022-12-13 14:27:09 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - block
+ * Reaturn: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while
+		(c <= 'z');
+	{
+		putchar(c);
+		c++;
+	}
+	putchar ('\n');
+	return (0);
+}
