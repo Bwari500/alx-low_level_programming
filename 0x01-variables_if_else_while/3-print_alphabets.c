fdf72b6ee726d3c076faaa30b826cb8dce8c2069
Bwari500 <dorisb.nyakundi@gmail.com>
2022-12-13 14:45:21 -0800

    print

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while
+		(c => 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	c = 'A';
+
+	while
+		 (c => 'z')
+{
+			 putchar(c);
+			  c++;
+}
+	putchar ('\n');
+	return (0);
+}
