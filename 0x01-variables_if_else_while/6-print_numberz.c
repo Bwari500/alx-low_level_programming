b360f6dc6b4f1f39dba38d24b9a234fd8a45a82e
Bwari500 <dorisb.nyakundi@gmail.com>
2022-12-13 15:13:53 -0800

    num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while
+		(c < 10)
+{
+	putchar(48 + 10);
+	c++;
+}
+putchar('\n');
+return (0);
+}
