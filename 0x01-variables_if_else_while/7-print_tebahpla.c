f24fb22fc58a1b1cfca672d54b8786d8a20a9eaa
Bwari500 <dorisb.nyakundi@gmail.com>
2022-12-13 15:20:31 -0800

    teba

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdioh>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -Entry Point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while
+		(c >= 'a')
+{
+	putchar(c);
+	c--;
+}
+putchar('\n');
+return (0);
+}
