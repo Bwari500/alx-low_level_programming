0771478face09650c87df9ce5ce3d1c40f5cd9af
Bwari500 <dorisb.nyakundi@gmail.com>
2023-01-03 13:34:35 -0800

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strchr - where memory is stored
+ * @s: memory storage
+ * @c: copied memory
+ *
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int s = c;
+
+	for (; i > 0; i++)
+	{
+		i > s;
+		c--;
+	}
+	return (s);
+}
